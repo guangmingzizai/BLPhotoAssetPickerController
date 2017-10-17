d48c11c21097265ce053f79fe9edd13d8ad2acb4
guangmingzizai <guangmingzizai@qq.com>
2017-10-17 12:27:55 +0800

    fix bugs

--- BLPhotoAssetPickerController/Classes/BLAssetSwitch.m
+++ BLPhotoAssetPickerController/Classes/BLAssetSwitch.m
@@ -17,13 +17,12 @@
 
 - (instancetype)initWithFrame:(CGRect)frame {
     if (self == [super initWithFrame:frame]) {
-        //self.backgroundColor = [UIColor redColor];
         NSString *title = NSLocalizedString(@"相机胶卷", nil);
         NSDictionary *attributes = @{NSFontAttributeName: [UIFont systemFontOfSize:16]};
         CGRect titleRect = [title boundingRectWithSize:CGSizeMake(CGFLOAT_MAX, 44) options:NSStringDrawingUsesLineFragmentOrigin attributes:attributes context:nil];
-        float width = titleRect.size.width;
-        CGSize labelSize = titleRect.size;
- 
+        float width = titleRect.size.width + 2;
+        CGSize labelSize = CGSizeMake(width, 44);
+
         _titleView = [[UIView alloc]init];
         _titleView.backgroundColor = [UIColor clearColor];
         _titleView.userInteractionEnabled = YES;
@@ -32,10 +31,9 @@
         [self addSubview:_titleView];
         [_titleView mas_makeConstraints:^(MASConstraintMaker *make) {
             make.size.mas_equalTo(CGSizeMake(width + 3 + 14, 44));
-            make.left.mas_equalTo(self.frame.size.width/2 - (titleRect.size.width+17)/2);
-            make.top.mas_equalTo(0);
+            make.center.mas_equalTo(self);
         }];
-        
+
         _titleLabel = [[UILabel alloc]init];
         _titleLabel.text = NSLocalizedString(@"相机胶卷", nil);
         _titleLabel.font = [UIFont boldSystemFontOfSize:16];
@@ -46,17 +44,17 @@
             make.centerY.mas_equalTo(_titleView.mas_centerY);
             make.size.mas_equalTo(labelSize);
         }];
-        
+
         _titleImageView = [[UIImageView alloc]initWithImage:[UIImage _imageForName:@"status_pic_down" inBundle:[NSBundle bundleForClass:[self class]]]];
         [_titleView addSubview:_titleImageView];
         [_titleImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.mas_equalTo(_titleLabel.mas_right).offset(3);
             make.right.mas_equalTo(0);
             make.centerY.mas_equalTo(_titleView.mas_centerY);
             make.size.mas_equalTo(CGSizeMake(14, 14));
         }];
         
         self.selectedOption = BLAssetSwitchPhotoPreviewAll;
-        
     }
     return self;
 }
@@ -106,9 +104,8 @@
                                            attributes:attributes
                                               context:nil];
     [_titleView mas_remakeConstraints:^(MASConstraintMaker *make) {
-        make.size.mas_equalTo(CGSizeMake(titleRect.size.width + 3 + 14, 44));
-        make.left.mas_equalTo(self.frame.size.width/2 - (titleRect.size.width+17)/2);
-        make.top.mas_equalTo(0);
+        make.size.mas_equalTo(CGSizeMake(titleRect.size.width + 2 + 3 + 14, 44));
+        make.center.mas_equalTo(self);
     }];
     
     [_titleLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
@@ -119,10 +116,21 @@
     }];
     
     [_titleImageView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(_titleLabel.mas_right).offset(3);
         make.right.mas_equalTo(0);
         make.centerY.mas_equalTo(_titleView.mas_centerY);
         make.size.mas_equalTo(CGSizeMake(14, 14));
     }];
+}
+
+- (CGSize)intrinsicContentSize {
+    NSDictionary *attributes = @{NSFontAttributeName: [UIFont systemFontOfSize:16]
+                                 ,NSForegroundColorAttributeName : [UIColor blackColor]};
+    CGRect titleRect = [_titleLabel.text boundingRectWithSize:CGSizeMake(CGFLOAT_MAX, 44)
+                                         options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
+                                      attributes:attributes
+                                         context:nil];
+    return CGSizeMake(titleRect.size.width + 2 + 3 + 14 + 2 * 8, 44);
 }
 
 @end
