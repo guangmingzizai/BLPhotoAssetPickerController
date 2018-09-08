e0b612b67c4451c7813dc5cf6acb4883f035c696
wangjianfei <wangjianfei@yipingfang.com>
2018-09-08 21:06:37 +0800

    remove Pods from version control

--- Example/Pods/Target Support Files/BLPhotoAssetPickerController/BLPhotoAssetPickerController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BLPhotoAssetPickerController : NSObject
-@end
-@implementation PodsDummy_BLPhotoAssetPickerController
-@end
--- Example/Pods/Target Support Files/MWPhotoBrowser@guangmingzizai/MWPhotoBrowser@guangmingzizai-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MWPhotoBrowser_guangmingzizai : NSObject
-@end
-@implementation PodsDummy_MWPhotoBrowser_guangmingzizai
-@end
